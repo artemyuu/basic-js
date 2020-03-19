46eb6c6076a6073f3a8774d5d0a696d64796388e
artemyuu <60568831+artemyuu@users.noreply.github.com>
2020-03-19 10:53:21 +0400

    feat: solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,40 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!(arr instanceof Array)) throw new Error();
+    
+    let op = arr.find(function(current){
+        return typeof current == 'string';
+    })
+    let index = arr.indexOf(op);
+        
+    switch(op){
+        case '--discard-prev':
+            if(index-1 >= 0){
+                delete arr[index];
+                delete arr[index-1];
+                return arr = arr.filter(el => el !== "");
+            } 
+        break;
+        case '--double-next':
+            if(index+1 < arr.length){
+                delete arr[index];
+                arr[index] = arr[index+1];
+                return arr;
+            } 
+        break;
+        case '--discard-next':
+             if(index+1 < arr.length){
+                delete arr[index];
+                delete arr[index+1];
+                return arr = arr.filter(el => el !== "");
+             }   
+
+        break;
+        case '--double-prev':
+             if(index-1 >= 0){
+                delete arr[index];
+                arr[index] = arr[index-1];
+                return arr;
+             }
+        break;
+    }
 };
