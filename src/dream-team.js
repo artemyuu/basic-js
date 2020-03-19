587ea9493df5bfd1995d87f84818d5bf8d8992c5
artemyuu <60568831+artemyuu@users.noreply.github.com>
2020-03-19 10:51:29 +0400

    feat: solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,20 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members)){
+        members.forEach(function(value, i){
+            if(typeof members[i] == "string"){
+                members[i] = value.trim();
+            }
+            else{
+                delete members[i];
+            }
+        });
+        members.sort();
+        members = members.reduce(function(result, value){
+            return result + value.slice(0,1);
+        },"");
+        return members.toUpperCase();
+    }
+    else {
+        return false;
+    }
 };
