f231579fef9f0a2e5b234ed7acf523b47fa42ea1
artemyuu <60568831+artemyuu@users.noreply.github.com>
2020-03-19 10:53:45 +0400

    feat: solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,105 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+        constructor(direct = true){
+        this.direct = true;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(msg, key) {
+        if (!msg || !key) throw new Error();
+
+        let codeSum = [];
+        let crypt = [];
+        msg = msg.toUpperCase().split('').map(function(x){
+            if(/[A-Z]/i.test(x) == true){
+                return x.charCodeAt(0)-65;
+            }
+            else{
+                return x.charCodeAt(0);
+            }
+        });
+        key = key.repeat(Math.ceil(msg.length / key.length)).toUpperCase().split('').map(function(x){
+            if(/[A-Z]/i.test(x) == true){
+                return x.charCodeAt(0)-65;
+            }
+            else{
+                return x.charCodeAt(0);
+            }
+        });
+        
+        for(let i = 0; i < msg.length; i++){
+            if(msg[i] < N && key[i] < N){
+                if((msg[i] + key[i]) >= N){
+                    codeSum[i] = (msg[i] + key[i]) - N;
+                }
+                else{
+                    codeSum[i] = (msg[i] + key[i]);
+                }
+            }
+            else{
+                codeSum[i] = msg[i];
+                key.splice(i,0,0);
+            }
+        }
+        crypt = codeSum.map(function(x){
+            if(x > N){
+                return String.fromCharCode(x);
+            }
+            else{
+                return String.fromCharCode(x + 65)
+            }
+            
+        });
+        if(!this.direct) crypt.reverse();
+        return crypt.join('');
+    }
+
+    decrypt(msg, key) {
+        if (!msg || !key) throw new Error();
+
+        let codeSum = [];
+        let crypt = [];
+        msg = msg.split('').map(function(x){
+            if(/[A-Z]/i.test(x) == true){
+                return x.charCodeAt(0)-65;
+            }
+            else{
+                return x.charCodeAt(0);
+            }
+        });
+
+        key = key.repeat(Math.ceil(msg.length / key.length)).toUpperCase().split('').map(function(x){
+            if(/[A-Z]/i.test(x) == true){
+                return x.charCodeAt(0)-65;
+            }
+            else{
+                return x.charCodeAt(0);
+            }
+        });
+        
+        for(let i = 0; i < msg.length; i++){
+            if(msg[i] < N && key[i] < N){
+                if((msg[i] - key[i]) < 0){
+                    codeSum[i] = (msg[i] - key[i]) + N;
+                }
+                else{
+                    codeSum[i] = (msg[i] - key[i]);
+                }
+            }
+            else{
+                codeSum[i] = msg[i];
+                key.splice(i,0,0);
+            }
+        }
+        crypt = codeSum.map(function(x){
+            if(x > N){
+                return String.fromCharCode(x);
+            }
+            else{
+                return String.fromCharCode(x + 65)
+            }
+            
+        });
+        if(!this.direct) crypt.reverse();
+        return crypt.join('');
     }
 }
 
